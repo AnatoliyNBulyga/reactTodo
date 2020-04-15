07bba5df80441cc02b6c4ab11449218dca66f2b1
AnatoliyNBulyga <anatoliynbulyga@gmail.com>
2020-04-15 20:26:40 +0300

    init todo

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, {useState, useEffect} from 'react';
+import TodoList from "./Todo/TodoList";
+import Context from "./context";
+import AddTodo from "./AddTodo";
+
 
 function App() {
+  const [todos, setTodos] = useState([]);
+  useEffect( () => {
+      fetch('https://jsonplaceholder.typicode.com/todos/?_limit=5')
+          .then(response => response.json())
+          .then( (todos) => {
+              setTimeout(() => {
+                  setTodos(todos);
+              }, 2000)
+
+          })
+  }, []);
+  const toggleTodo = (id) => {
+      setTodos(todos.map( item => {
+              if (item.id === id) {
+                  item.completed = !item.completed
+              }
+              return item;
+          }))
+
+  };
+  const onRemoveItem = (id) => {
+      setTodos(todos.filter( item => item.id !== id ));
+  };
+  const onCreate = (value) => {
+      setTodos([
+          ...todos,
+          {id: todos.length + 1, completed: false, title: value}
+      ]);
+  }
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+      <Context.Provider value={{
+          onRemoveItem: onRemoveItem
+      }}>
+          <div className="wrapper">
+              <h1>React tutorial</h1>
+              <AddTodo onCreate={onCreate} />
+              {
+                 todos.length
+                 ? <TodoList todos={todos} onChangeItem={toggleTodo} onRemoveItem={onRemoveItem}/>
+                 : <p>Нет заданий</p>
+              }
+
+          </div>
+      </Context.Provider>
+
   );
 }
 
---
+++ src/Loader/Loader.js
@@ -0,0 +1,8 @@
+import React from "react";
+import classes from "Loader.module.css";
+
+const Loader = () => (
+    <div className={classes.ldsDualRing}></div>
+);
+
+export default Loader;
